695ee8024f5749c22381b2abbf7f4fff1b742900
Zhenhui Xu <sleepym09@163.com>
2024-03-06 21:24:06 +0800

    add compose navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,34 +55,35 @@ dependencies {
     val constraintVersion = "2.1.4"
     val activityVersion = "1.8.2"
     val materialVersion = "1.11.0"
-    implementation("androidx.core:core-ktx:${coreVersion}")
-    implementation("androidx.appcompat:appcompat:${appcompatVersion}")
-    implementation("androidx.constraintlayout:constraintlayout:${constraintVersion}")
-    implementation("androidx.activity:activity-compose:${activityVersion}")
-    implementation("com.google.android.material:material:${materialVersion}")
+    val navigationVersion = "2.7.7"
+    implementation("androidx.core:core-ktx:$coreVersion")
+    implementation("androidx.appcompat:appcompat:$appcompatVersion")
+    implementation("androidx.constraintlayout:constraintlayout:$constraintVersion")
+    implementation("androidx.activity:activity-compose:$activityVersion")
+    implementation("androidx.navigation:navigation-compose$navigationVersion")
+    implementation("com.google.android.material:material:$materialVersion")
 
     // coroutines
     val coroutinesVersion = "1.8.0"
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${coroutinesVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
     // compose
     val composeBomVersion = "2024.02.01"
-    implementation(platform("androidx.compose:compose-bom:${composeBomVersion}"))
+    implementation(platform("androidx.compose:compose-bom:$composeBomVersion"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose")
 
     // lifecycle
     val lifecycleVersion = "2.7.0"
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${lifecycleVersion}")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:${lifecycleVersion}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
 
     // test
     val junitVersion = "4.13.2"
-    testImplementation("junit:junit:${junitVersion}")
+    testImplementation("junit:junit:$junitVersion")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")

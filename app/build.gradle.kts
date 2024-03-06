a0c31af4af2975a3f0b478640954e1d11e069689
Zhenhui Xu <sleepym09@163.com>
2024-03-06 21:15:38 +0800

    add compose navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose")
 
     // lifecycle
     val lifecycleVersion = "2.7.0"
